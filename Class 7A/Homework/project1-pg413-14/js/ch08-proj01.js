ffcbdeb81ff9490c78b05c3fb09503f2030de1bd
hrabenstein <drabenstein@wits.edu>
2024-03-12 15:38:34 -0400

    Homework 7

--- Class 7A/Homework/project1-pg413-14/js/ch08-proj01.js
+++ Class 7A/Homework/project1-pg413-14/js/ch08-proj01.js
@@ -1,6 +1,24 @@
 
-const tax_rate = prompt('Enter tax rate (0.10)');
-const shipping_threshold = prompt('Enter shipping threshold (1000)');
+const tax_rate = prompt('Enter tax rate');
+const shipping_threshold = prompt('Enter shipping threshold');
+let subtotal = 0;
 
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
+
+   for(let item of cart)
+   {
+      outputCartRow(item, calculateTotal(item.product.price, item.quantity));
+   }
+
+   for (let item of cart)
+   {
+      // calculatesubtotal(item);
+      subtotal = subtotal + (item.product.price * item.quantity);
+   }
+
+document.write('<tr class="totals"> <td colspan="4"> Subtotal </td> <td>$' + subtotal.toFixed(2) + '</td> </tr>');
+
+document.write('<tr class="totals"> <td colspan="4">Tax</td> <td>$' + (tax_rate * subtotal) + '</td> </tr>');
+
+document.write('<tr class="totals"><td colspan="4">Shipping</td><td>$' + calculateShipping(shipping_threshold, subtotal) + '</td> </tr>');
--- Class 7A/Homework/project1-pg413-14/js/functions.js
+++ Class 7A/Homework/project1-pg413-14/js/functions.js
@@ -1,4 +1,36 @@
-/* define your functions here */
+
+/* define your functions here 
+Dassi Rabenstein
+3/10/24 */
+
+function calculateTotal(quantity, price)
+{
+    return (quantity * price);
+}
+
+//using data from the data.js file, this displays the data
+function outputCartRow(item, total) 
+{
+    document.write('<tr><td><img src=images/' + item.product.filename + '></td><td>' + item.product.title + '</td><td>' + item.quantity + '</td><td>'+ item.product.price.toFixed(2) +'</td><td>'+ total.toFixed(2) +'</td></tr>');
+}
+
+// function calculatesubtotal(item)
+// {
+//     subtotal = subtotal + (item.product.price * item.quantity);
+//     return subtotal;
+// }
+
+function calculateShipping(shipping_threshold, subtotal)
+{
+    if (subtotal > shipping_threshold)
+    {
+        return 0.00;
+    }
+    else
+    {
+        return 40.00;
+    }
+}
 
 
 
